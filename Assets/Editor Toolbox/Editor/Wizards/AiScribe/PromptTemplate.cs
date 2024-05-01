415e19084e3ef378cdcb71fcad9bb50ec66cc29f
naschenbach <naschenbach@fb.com>
2024-05-01 16:03:39 -0700

    AIScribe updates

---
+++ Assets/Editor Toolbox/Editor/Wizards/AiScribe/PromptTemplate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = FileName, menuName = MenuName)]
+public class PromptTemplate : ScriptableObject
+{
+    public const string FileName = nameof(AiScribe) + " Prompt";
+    public const string MenuName = nameof(AiScribe) + "/New Prompt Templates";
+    public string text = "Enter your custom prompt here...";
+    public AiScribe.Options options;
+
+    [CustomEditor(typeof(PromptTemplate))]
+    public class PromptTemplateEditor : Editor
+    {
+        private Vector2 scrollPosition;
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("options"));
+
+            EditorGUILayout.Space();
+
+            SerializedProperty textProp = serializedObject.FindProperty("text");
+            GUIStyle textAreaStyle = new GUIStyle(GUI.skin.textArea) { wordWrap = true };
+            EditorGUI.BeginChangeCheck();
+            string newText = EditorGUILayout.TextArea(textProp.stringValue, textAreaStyle, GUILayout.ExpandHeight(true));
+            if (EditorGUI.EndChangeCheck())
+                textProp.stringValue = newText;
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    // Hack to allow an empty first entry in the "Select Prompt Template" dropdown
+    public string Title => this.name;
+
+}
