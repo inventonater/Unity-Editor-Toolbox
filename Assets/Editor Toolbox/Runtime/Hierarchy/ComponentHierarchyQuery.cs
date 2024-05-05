03fe842a4f53ba299e682106dab033abe129137e
Nathan Aschenbach <naschenbach@fb.com>
2024-05-05 16:59:13 -0700

    ResolveIn deleted!  ComponentHierarchyQuery can accept custom traversal algorithms

--- Assets/Editor Toolbox/Runtime/Hierarchy/ComponentHierarchyQuery.cs
+++ Assets/Editor Toolbox/Runtime/Hierarchy/ComponentHierarchyQuery.cs
@@ -11,23 +11,113 @@ namespace Toolbox
     {
         public static ComponentHierarchyQuery<T> GetAncestors<T>(this Component component) where T : Component
         {
-            return component.HierarchyQuery<T>().WithTraversal(a => a.TraversalAlgorithm = TraversalAlgorithm.Ancestors);
+            return component.HierarchyQuery<T>().WithTraversal(a => a.TraversalAlgorithm = HierarchyTraversalAlgorithms<T>.Get(ETraversalAlgorithm.Ancestors));
         }
 
         public static ComponentHierarchyQuery<T> GetDescendants<T>(this Component component) where T : Component
         {
-            return component.HierarchyQuery<T>().WithTraversal(a => a.TraversalAlgorithm = TraversalAlgorithm.DescendantsBreadthFirst);;
+            return component.HierarchyQuery<T>().WithTraversal(a => a.TraversalAlgorithm = HierarchyTraversalAlgorithms<T>.Get(ETraversalAlgorithm.DescendantsBreadthFirst));
+        }
+
+        public static ComponentHierarchyQuery<T> GetScene<T>(this Component component) where T : Component
+        {
+            return component.HierarchyQuery<T>().WithTraversal(a => a.TraversalAlgorithm = HierarchyTraversalAlgorithms<T>.Get(ETraversalAlgorithm.EntireScene));
         }
 
         public static ComponentHierarchyQuery<T> HierarchyQuery<T>(this Component component) where T : Component => new(component);
     }
 
-    public enum TraversalAlgorithm
+    public enum ETraversalAlgorithm
     {
         DescendantsBreadthFirst,
         DescendantsDepthFirst,
         Ancestors,
         SameTreeLevel,
+        EntireScene
+    }
+
+    public static class HierarchyTraversalAlgorithms<T> where T : Component
+    {
+        public static TraversalAlgorithm<T> Get(ETraversalAlgorithm algorithm)
+        {
+            return algorithm switch
+            {
+                ETraversalAlgorithm.DescendantsBreadthFirst => TraverseDescendantsBreadthFirst,
+                ETraversalAlgorithm.DescendantsDepthFirst => TraverseDescendantsDepthFirst,
+                ETraversalAlgorithm.Ancestors => TraverseAncestors,
+                ETraversalAlgorithm.SameTreeLevel => TraverseSameTreeLevel,
+                ETraversalAlgorithm.EntireScene => TraverseEntireScene,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public static IEnumerable<T> TraverseDescendantsBreadthFirst(ComponentHierarchyQuery<T> query)
+        {
+            var queue = new Queue<Transform>();
+            queue.Enqueue(query.Origin.transform); // Start from the origin object so we can potentially include it
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (query.Traversal.ShouldHaltTraversal(current)) continue;
+                foreach (Transform child in current) queue.Enqueue(child);
+                foreach (var component in VisitComponents(current, query)) yield return component;
+            }
+        }
+
+        public static IEnumerable<T> TraverseDescendantsDepthFirst(ComponentHierarchyQuery<T> query)
+        {
+            var stack = new Stack<Transform>();
+            stack.Push(query.Origin.transform); // Start from the origin object so we can potentially include it
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                if (query.Traversal.ShouldHaltTraversal(current)) continue;
+                foreach (var component in VisitComponents(current, query)) yield return component;
+                for (int i = current.childCount - 1; i >= 0; i--) stack.Push(current.GetChild(i));
+            }
+        }
+
+        public static IEnumerable<T> TraverseAncestors(ComponentHierarchyQuery<T> query)
+        {
+            var current = query.Origin.transform; // Start from the origin object so we can potentially include it
+            while (current != null)
+            {
+                if (query.Traversal.ShouldHaltTraversal(current)) yield break;
+                foreach (var component in VisitComponents(current, query)) yield return component;
+                current = current.parent;
+            }
+        }
+
+        public static IEnumerable<T> TraverseSameTreeLevel(ComponentHierarchyQuery<T> query)
+        {
+            var sameLevel = query.Origin.transform.parent != null ? query.Origin.transform.parent.Cast<Transform>() : Hierarchy.GetRootGameObject();
+            foreach (Transform current in sameLevel)
+            {
+                if (query.Traversal.ShouldHaltTraversal(current)) continue; // Skip objects based on custom criteria
+                foreach (var component in VisitComponents(current, query)) yield return component;
+            }
+        }
+
+        public static IEnumerable<T> TraverseEntireScene(ComponentHierarchyQuery<T> query)
+        {
+            foreach (var component in GameObject.FindObjectsOfType<T>())
+            {
+                if (query.Traversal.ShouldSkip(component.gameObject)) continue;
+                if (query.Traversal.ShouldSkip(component)) continue;
+                if (query.Filter.MatchesFilters(query.Origin, component)) yield return component;
+            }
+        }
+
+        public static IEnumerable<T> VisitComponents(Transform transform, ComponentHierarchyQuery<T> query)
+        {
+            if (query.Traversal.ShouldSkip(transform.gameObject)) yield break;
+
+            foreach (var visit in transform.GetComponents<T>())
+            {
+                if (query.Traversal.ShouldSkip(visit)) continue;
+                if (query.Filter.MatchesFilters(query.Origin, visit)) yield return visit;
+            }
+        }
     }
 
     [Flags]
@@ -42,10 +132,20 @@ namespace Toolbox
         HaltAllInactive = HaltInactiveComponents | HaltInactiveGameObjects
     }
 
-    public class HierarchyTraversalRules
+    public class HierarchyTraversalRules<T> where T : Component
     {
-        public TraversalAlgorithm TraversalAlgorithm { get; set; } = TraversalAlgorithm.DescendantsBreadthFirst;
+        public TraversalAlgorithm<T> TraversalAlgorithm { get; set; }
         public InactiveTraversal InactiveTraversal { get; set; } = InactiveTraversal.SkipAllInactive;
+
+        public bool ShouldSkip(GameObject gameObject) => !gameObject.activeInHierarchy && InactiveTraversal.HasFlag(InactiveTraversal.SkipInactiveGameObjects);
+
+        public bool ShouldSkip(T component)
+        {
+            if (component is Behaviour behaviour && !behaviour.enabled) return InactiveTraversal.HasFlag(InactiveTraversal.SkipInactiveComponents);
+            return false;
+        }
+
+        public bool ShouldHaltTraversal(Transform transform) => !transform.gameObject.activeInHierarchy && InactiveTraversal.HasFlag(InactiveTraversal.HaltInactiveGameObjects);
     }
 
     public class ComponentFilterRules<T> where T : Component
@@ -56,27 +156,38 @@ namespace Toolbox
         public string NameContains { get; set; }
         public Regex NameRegex { get; set; }
         public Func<T, bool> CustomFilter { get; set; }
+
+        public bool MatchesFilters(Component origin, T component)
+        {
+            if (origin == component && !AllowOriginComponent) return false;
+            if (origin.gameObject == component.gameObject && !AllowOriginGameObject) return false;
+            if (!string.IsNullOrEmpty(NameIs) && component.name != NameIs) return false;
+            if (!string.IsNullOrEmpty(NameContains) && !component.name.Contains(NameContains)) return false;
+            if (NameRegex != null && !NameRegex.IsMatch(component.name)) return false;
+            if (CustomFilter != null && !CustomFilter(component)) return false;
+            return true;
+        }
     }
+
+    public delegate IEnumerable<T> TraversalAlgorithm<T>(ComponentHierarchyQuery<T> query) where T : Component;
 
     public class ComponentHierarchyQuery<T> : IEnumerable<T> where T : Component
     {
-        private readonly Component _origin;
-
+        public Component Origin { get; }
+        public HierarchyTraversalRules<T> Traversal { get; set; } = new HierarchyTraversalRules<T>();
+        public ComponentFilterRules<T> Filter { get; set; } = new ComponentFilterRules<T>();
         public ComponentHierarchyQuery(Component origin)
         {
-            _origin = origin;
+            Origin = origin;
         }
 
-        public HierarchyTraversalRules Traversal { get; set; } = new HierarchyTraversalRules();
-        public ComponentFilterRules<T> Filter { get; set; } = new ComponentFilterRules<T>();
-
-        public ComponentHierarchyQuery<T> WithTraversal(Action<HierarchyTraversalRules> action)
+        public ComponentHierarchyQuery<T> WithTraversal(Action<HierarchyTraversalRules<T>> action)
         {
             action(Traversal);
             return this;
         }
 
-        public ComponentHierarchyQuery<T> WithTraversal(HierarchyTraversalRules rules)
+        public ComponentHierarchyQuery<T> WithTraversal(HierarchyTraversalRules<T> rules)
         {
             Traversal = rules;
             return this;
@@ -96,106 +207,6 @@ namespace Toolbox
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         public IEnumerator<T> GetEnumerator() => SelectEnumerable().GetEnumerator();
-
-        private IEnumerable<T> SelectEnumerable()
-        {
-            return Traversal.TraversalAlgorithm switch
-            {
-                TraversalAlgorithm.DescendantsBreadthFirst => TraverseDescendantsBreadthFirst(),
-                TraversalAlgorithm.DescendantsDepthFirst => TraverseDescendantsDepthFirst(),
-                TraversalAlgorithm.Ancestors => TraverseAncestors(),
-                TraversalAlgorithm.SameTreeLevel => TraverseSameTreeLevel(),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-        }
-
-        private IEnumerable<T> TraverseDescendantsBreadthFirst()
-        {
-            var queue = new Queue<Transform>();
-            queue.Enqueue(_origin.transform); // Start from the origin object so we can potentially include it
-            while (queue.Count > 0)
-            {
-                var current = queue.Dequeue();
-                if (ShouldHaltTraversal(current)) continue;
-                foreach (Transform child in current) queue.Enqueue(child);
-                foreach (var component in VisitComponents(current)) yield return component;
-            }
-        }
-
-        private IEnumerable<T> TraverseDescendantsDepthFirst()
-        {
-            var stack = new Stack<Transform>();
-            stack.Push(_origin.transform); // Start from the origin object so we can potentially include it
-            while (stack.Count > 0)
-            {
-                var current = stack.Pop();
-                if (ShouldHaltTraversal(current)) continue;
-                foreach (var component in VisitComponents(current)) yield return component;
-                for (int i = current.childCount - 1; i >= 0; i--) stack.Push(current.GetChild(i));
-            }
-        }
-
-        private IEnumerable<T> TraverseAncestors()
-        {
-            var current = _origin.transform; // Start from the origin object so we can potentially include it
-            while (current != null)
-            {
-                if (ShouldHaltTraversal(current)) yield break;
-                foreach (var component in VisitComponents(current)) yield return component;
-                current = current.parent;
-            }
-        }
-
-        private IEnumerable<T> TraverseSameTreeLevel()
-        {
-            var sameLevel = _origin.transform.parent != null ? _origin.transform.parent.Cast<Transform>() : Hierarchy.GetRootGameObject();
-            foreach (Transform current in sameLevel)
-            {
-                if (ShouldHaltTraversal(current)) continue;  // Skip objects based on custom criteria
-                foreach (var component in VisitComponents(current)) yield return component;
-            }
-        }
-
-        private IEnumerable<T> VisitComponents(Transform transform)
-        {
-            if (ShouldSkip(transform.gameObject)) yield break;
-
-            foreach (var component in transform.GetComponents<T>())
-            {
-                if (ShouldSkip(component)) continue;
-                if (MatchesFilters(component)) yield return component;
-            }
-        }
-
-        private bool ShouldSkip(GameObject gameObject)
-        {
-            return !gameObject.activeInHierarchy && Traversal.InactiveTraversal.HasFlag(InactiveTraversal.SkipInactiveGameObjects);
-        }
-
-        private bool ShouldSkip(T component)
-        {
-            if (component is Behaviour behaviour && !behaviour.enabled)
-            {
-                return Traversal.InactiveTraversal.HasFlag(InactiveTraversal.SkipInactiveComponents);
-            }
-
-            return false;
-        }
-
-        private bool ShouldHaltTraversal(Transform transform)
-        {
-            return !transform.gameObject.activeInHierarchy && Traversal.InactiveTraversal.HasFlag(InactiveTraversal.HaltInactiveGameObjects);
-        }
-
-        private bool MatchesFilters(T component)
-        {
-            if (_origin == component && !Filter.AllowOriginComponent) return false;
-            if (_origin.gameObject == component.gameObject && !Filter.AllowOriginGameObject) return false;
-            if (!string.IsNullOrEmpty(Filter.NameIs) && component.name != Filter.NameIs) return false;
-            if (!string.IsNullOrEmpty(Filter.NameContains) && !component.name.Contains(Filter.NameContains)) return false;
-            if (Filter.NameRegex != null && !Filter.NameRegex.IsMatch(component.name)) return false;
-            if (Filter.CustomFilter != null && !Filter.CustomFilter(component)) return false;
-            return true;
-        }
+        private IEnumerable<T> SelectEnumerable() => Traversal.TraversalAlgorithm(this);
     }
 }
